508d18816b9899c9c00abeac288e35e1068d624b
BoIv1412 <bojan.ivanovski@studentambassadors.com>
2024-02-13 02:46:30 +0100

    backend

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     storage_account_name = "storagebackend12"
     container_name       = "tfstate"
-    key                  = "terraform.tfstate"
+    key                  = "Terraform/terraform.tfstate"
     resource_group_name  = "Marathon1"
   }
 }
