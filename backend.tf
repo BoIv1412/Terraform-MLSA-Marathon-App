b28504fab1840e9b765d8bfad242396391dfa34e
BoIv1412 <bojan.ivanovski@studentambassadors.com>
2024-02-13 02:28:35 +0100

    backend file added

--- backend.tf
+++ backend.tf
@@ -3,5 +3,6 @@ terraform {
     storage_account_name = "storagebackend12"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
+    resource_group_name  = "marathon"
   }
 }
