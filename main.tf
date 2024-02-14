1cc3633381aebeb394881880cf74187719619fe1
BoIv1412 <bojan.ivanovski@studentambassadors.com>
2024-02-14 01:33:37 +0100

    added storage account

--- main.tf
+++ main.tf
@@ -20,6 +20,20 @@ resource "azurerm_resource_group" "marathon" {
   location = "East US"
 }
 
+resource "azurerm_storage_account" "storageacc" {
+  name                     = "storagebackend12"
+  resource_group_name      = azurerm_resource_group.marathon.name
+  location                 = azurerm_resource_group.marathon.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "storage_comtainer" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.storageacc.name
+  container_access_type = "container" # Set the access type (e.g., private, blob, container)
+}
+
 resource "azurerm_service_plan" "marathon_service_plan" {
   name                = "marathon_service_plan"
   resource_group_name = azurerm_resource_group.marathon.name
