c9c823729086511604d8f0f49b86ac4a117740fe
BoIv1412 <bojan.ivanovski@studentambassadors.com>
2024-02-18 22:57:14 +0100

    removed credentials

--- main.tf
+++ main.tf
@@ -13,10 +13,6 @@ terraform {
 provider "azurerm" {
   skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
 }
 
 data "azurerm_client_config" "current" {}
