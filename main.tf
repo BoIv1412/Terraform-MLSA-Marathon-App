cde28b57a68a91e63a138c55e15c313e238695d8
BoIv1412 <bojan.ivanovski@studentambassadors.com>
2024-02-18 13:00:59 +0100

    update

--- main.tf
+++ main.tf
@@ -8,13 +8,17 @@ terraform {
     }
   }
 }
+
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
-  features {}
+  features {
+
+  }
 }
 
 data "azurerm_client_config" "current" {}
+
 # Create a resource group
 resource "azurerm_resource_group" "marathon" {
   name     = "Marathon1"
@@ -41,8 +45,8 @@ resource "azurerm_service_plan" "marathon_service_plan" {
   location            = azurerm_resource_group.marathon.location
   sku_name            = "F1"
   os_type             = "Windows"
-
 }
+
 resource "azurerm_windows_web_app" "marathon_api" {
   name                = "marathon-api"
   resource_group_name = azurerm_resource_group.marathon.name
@@ -52,12 +56,9 @@ resource "azurerm_windows_web_app" "marathon_api" {
   site_config {
     always_on = false
     application_stack {
-
       current_stack  = "dotnet"
       dotnet_version = "v7.0"
-
     }
-
   }
 
   connection_string {
@@ -71,7 +72,6 @@ resource "azurerm_windows_web_app" "marathon_api" {
     type  = "RedisCache"
     value = azurerm_redis_cache.redis.primary_connection_string
   }
-
 }
 
 resource "azurerm_windows_web_app" "marathon_client" {
@@ -91,10 +91,7 @@ resource "azurerm_windows_web_app" "marathon_client" {
   app_settings = {
     "DATABASE_CONNECTION_STRING" = "Server=tcp:${azurerm_mssql_server.mssql_server.name}.database.windows.net,1433;Initial Catalog=${azurerm_mssql_database.mssql_database.name};Persist Security Info=False;User ID=${azurerm_mssql_server.mssql_server.administrator_login};Password=${azurerm_mssql_server.mssql_server.administrator_login_password};MultipleActiveResultSets=True;Encrypt=True",
     "REDIS_CONNECTION_STRING"    = "redis://${azurerm_redis_cache.redis.hostname}.redis.cache.windows.net:${azurerm_redis_cache.redis.ssl_port}?ssl=true&sslverify=false"
-
   }
-
-
 }
 
 resource "azurerm_mssql_server" "mssql_server" {
@@ -119,7 +116,6 @@ resource "azurerm_mssql_database" "mssql_database" {
   lifecycle {
     prevent_destroy = true
   }
-
 }
 
 resource "azurerm_redis_cache" "redis" {
@@ -144,33 +140,36 @@ resource "azurerm_redis_firewall_rule" "redis_firewall" {
   end_ip              = "255.255.255.255"
 }
 
+resource "azurerm_mssql_firewall_rule" "dbfirewall" {
+  name             = "FirewallRule1"
+  server_id        = azurerm_mssql_server.mssql_server.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "255.255.255.255"
+}
+
 resource "azurerm_key_vault" "key_vault" {
   name                     = "keyvault125347"
   location                 = azurerm_resource_group.marathon.location
   resource_group_name      = azurerm_resource_group.marathon.name
   tenant_id                = data.azurerm_client_config.current.tenant_id
   purge_protection_enabled = false
+  sku_name                 = "standard"
+}
 
-  sku_name = "standard"
+data "azuread_service_principal" "terraform" {
+  display_name = "MarathonT"
+}
 
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
-    key_permissions = [
-      "Get",
-    ]
-    secret_permissions = [
-      "Get", "Backup", "Delete", "List", "Purge", "Recover", "Restore", "Set",
-    ]
-    storage_permissions = [
-      "Get",
-    ]
-  }
+resource "azurerm_key_vault_access_policy" "terraform_policy" {
+  key_vault_id       = azurerm_key_vault.key_vault.id
+  tenant_id          = data.azurerm_client_config.current.tenant_id
+  object_id          = data.azuread_service_principal.terraform.object_id
+  secret_permissions = ["Backup", "Delete", "Get", "List", "Purge", "Recover", "Restore", "Set"]
 }
 
 resource "azurerm_key_vault_secret" "key_vault_secret1" {
   name         = "default-connection-string"
-  value        = "Server=tcp:${azurerm_mssql_server.mssql_server.name},1433;Initial Catalog=${azurerm_mssql_database.mssql_database.name};Persist Security Info=False;User ID=${azurerm_mssql_server.mssql_server.administrator_login};Password=${azurerm_mssql_server.mssql_server.administrator_login_password};MultipleActiveResultSets=True;Encrypt=True"
+  value        = "Server=tcp:${azurerm_mssql_server.mssql_server.name}.database.windows.net,1433;Initial Catalog=${azurerm_mssql_database.mssql_database.name};Persist Security Info=False;User ID=${azurerm_mssql_server.mssql_server.administrator_login}@${azurerm_mssql_server.mssql_server.name};Password=${azurerm_mssql_server.mssql_server.administrator_login_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
   key_vault_id = azurerm_key_vault.key_vault.id
 }
 
@@ -179,5 +178,4 @@ resource "azurerm_key_vault_secret" "key_vault_secret2" {
   value        = azurerm_redis_cache.redis.primary_connection_string
   key_vault_id = azurerm_key_vault.key_vault.id
 }
-
 
