074b90ab0589734d868e7b7973e08a875002e364
BoIv1412 <bojan.ivanovski@studentambassadors.com>
2024-02-23 02:14:27 +0100

    updates

--- variables.tf
+++ variables.tf
@@ -88,7 +88,3 @@ variable "redis_firewall" {
   default     = "FirewallRule1"
 }
 
-variable "AZURE_CLIENT_ID" {}
-variable "AZURE_TENANT_ID" {}
-variable "AZURE_CLIENT_SECRET" {}
-variable "AZURE_SUBSCRIPTION_ID" {}
