752a2493f01d2396cccfa110303441b0f25234f9
BoIv1412 <140695248+BoIv1412@users.noreply.github.com>
2024-12-03 23:19:48 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -106,7 +106,6 @@ resource "azurerm_redis_cache" "redis" {
   capacity            = 2
   family              = "C"
   sku_name            = "Basic"
-  enable_non_ssl_port = false
   minimum_tls_version = "1.2"
 
   redis_configuration {
